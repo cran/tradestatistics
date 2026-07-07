cd84e05d2fa82224f92e0b938d0b465836a7c1ab
Mauricio Vargas <m.vargas.sepulveda@gmail.com>
2026-07-07 21:00:02 +0000

    version 7.0.0

--- inst/doc/basic-usage.R
+++ inst/doc/basic-usage.R
@@ -8,91 +8,61 @@ knitr::opts_chunk$set(
 
 ## ----pkgs---------------------------------------------------------------------
 library(tradestatistics)
-library(tibble)
 
 ## ----tables, eval = T---------------------------------------------------------
-as_tibble(ots_tables)
+ots_tables
 
 ## ----countries, eval = T------------------------------------------------------
-as_tibble(ots_countries)
+ots_countries
 
 ## ----commodities, eval = T----------------------------------------------------
-as_tibble(ots_commodities)
+ots_sectors
 
-## ----inflation, eval = T------------------------------------------------------
-as_tibble(ots_gdp_deflator)
+ots_industries
 
 ## ----country_code-------------------------------------------------------------
 # Single match with no replacement
-as_tibble(ots_country_code("Chile"))
+ots_country_code("Chile")
 
 # Single match with replacement
-as_tibble(ots_country_code("America"))
+ots_country_code("America")
 
 # Double match with no replacement
-as_tibble(ots_country_code("Germany"))
+ots_country_code("Germany")
 
 ## ----commodity_code2----------------------------------------------------------
-as_tibble(ots_commodity_code(commodity = " Horse ", section = " ANIMAL "))
+ots_sector_code(" AgriCulture ")
+ots_industry_code("  CeReaLS")
 
-## ----yrpc1, eval = F----------------------------------------------------------
-# yrpc <- ots_create_tidy_data(
+## ----aggregate1, eval = F-----------------------------------------------------
+# ots_create_tidy_data(
 #   years = 2019,
-#   reporters = "chl",
-#   partners = "arg",
-#   table = "yrpc"
+#   importers = "chl",
+#   exporters = "arg",
+#   table = "itpde_imp_exp"
 # )
-# 
-# as_tibble(yrpc)
 
-## ----yrpc3, eval = F----------------------------------------------------------
-# # Note that here I'm passing Peru and not per which is the ISO code for Peru
-# # The same applies to Brazil
-# yrpc2 <- ots_create_tidy_data(
+## ----aggregate2, eval = F-----------------------------------------------------
+# ots_create_tidy_data(
 #   years = 2018:2019,
-#   reporters = c("chl", "Peru", "bol"),
-#   partners = c("arg", "Brazil"),
-#   commodities = c("01", "food"),
-#   table = "yrpc"
+#   importers = c("chl", "Peru", "col"),
+#   exporters = c("arg", "Brazil"),
+#   table = "itpde_imp_exp"
 # )
 
-## ----yrp3, eval = F-----------------------------------------------------------
-# yrp <- ots_create_tidy_data(
+## ----sector1, eval = F--------------------------------------------------------
+# ots_create_tidy_data(
 #   years = 2018:2019,
-#   reporters = c("chl", "per"),
-#   partners = "arg",
-#   table = "yrp"
+#   importers = c("chl", "Peru", "col"),
+#   exporters = c("arg", "Brazil"),
+#   table = "itpde_imp_exp_sec"
 # )
 
-## ----yrc2, eval = F-----------------------------------------------------------
-# yrc <- ots_create_tidy_data(
-#   years = 2019,
-#   reporters = "chl",
-#   commodities = "010121",
-#   table = "yrc"
-# )
-
-## ----yr2, eval = F------------------------------------------------------------
-# yr <- ots_create_tidy_data(
+## ----industry1, eval = F------------------------------------------------------
+# ots_create_tidy_data(
 #   years = 2018:2019,
-#   reporters = c("chl", "arg", "per"),
-#   table = "yr"
+#   importers = c("chl", "Peru", "col"),
+#   exporters = c("arg", "Brazil"),
+#   table = "itpde_ind"
 # )
-
-## ----yc1, eval = F------------------------------------------------------------
-# yc <- ots_create_tidy_data(
-#   years = 2019,
-#   table = "yc"
-# )
-
-## ----yc2, eval = F------------------------------------------------------------
-# yc2 <- ots_create_tidy_data(
-#   years = 2019,
-#   commodities = "010121",
-#   table = "yc"
-# )
-
-## ----inflation2, eval=FALSE---------------------------------------------------
-# inflation <- ots_gdp_deflator_adjustment(yr, reference_year = 2000)
-# as_tibble(inflation)
 
